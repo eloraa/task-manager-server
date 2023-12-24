aba9c51a0a9e1eab7c1fc8c4c47cf8639271b167
elora <108570316+eloraistired@users.noreply.github.com>
2023-12-24 14:15:46 +0000

    make value non required

--- api/validations/task.validation.js
+++ api/validations/task.validation.js
@@ -17,10 +17,10 @@ module.exports = {
     },
     update: {
         body: Joi.object({
-            title: Joi.string().min(1).max(100).required(),
-            description: Joi.string().min(1).max(300).required(),
-            date: Joi.string().required(),
-            priority: Joi.string().valid("low", "moderate", "high").required(),
+            title: Joi.string().min(1).max(100),
+            description: Joi.string().min(1).max(300),
+            date: Joi.string(),
+            priority: Joi.string().valid("low", "moderate", "high"),
             index: Joi.number().integer(),
         }),
     },
